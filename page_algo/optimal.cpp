5526810e5e08a598828997231e6e8f3c4dfc26e6
Atharv Vyas <95745167+Mr-okey@users.noreply.github.com>
2022-12-20 21:04:14 -0800

    Update optimal.cpp

--- page_algo/optimal.cpp
+++ page_algo/optimal.cpp
@@ -1,91 +1,119 @@
-// CPP program to demonstrate optimal page
-// replacement algorithm.
-#include <bits/stdc++.h>
+// Optimal
+
+#include<bits/stdc++.h>
 using namespace std;
+int main(){
+    int n,m,i,j,k;
+    cout<<"Enter number of frames\n";
+    cin>>n;
+    cout<<"Enter number of processes\n";
+    cin>>m;
+    vector<int> p(m);
+    cout<<"Enter processes\n";
+    for(i=0;i<m;i++){
+        cin>>p[i];
+    }
+    vector<vector<int>> a(n,vector<int>(m,-1));
+    map <int, int> mp;    
+    for(i=0;i<m;i++){
+        vector<int> op;
+        vector<pair<int,int>> c;
+        for(auto q: mp){
+            c.push_back({q.second,q.first});
+        }
+        for(int q=i+1;q<m;q++){
+            for(j=0;j<n;j++){
+                if(a[j][i]==p[q]){
+                    op.push_back(p[q]);
+                }            
+            }
+        }
+        sort(op.begin(),op.end());
+        op.erase(unique(op.begin(),op.end()),op.end());
+        bool dontCall=true;        
+        if(op.size()==n){
+            dontCall=false;
+        }
+        sort(c.begin(),c.end());
+        bool hasrun=false;
+        for(j=0;j<n;j++){
+            if(a[j][i]==p[i]){
+                mp[p[i]]++;
+                hasrun=true;
+                break;
+            }
+            if(a[j][i]==-1){
+                for(k=i;k<m;k++)
+                    a[j][k]=p[i];
+                mp[p[i]]++;
+                hasrun=true;
+                break;
+            }
+        }
+        if(j==n||hasrun==false){
+            for(j=0;j<n;j++){
+                if(dontCall==true){
+                    if(a[j][i]==c[c.size()-1].second){
+                        mp.erase(a[j][i]);
+                        for(k=i;k<m;k++)
+                            a[j][k]=p[i];
+                        mp[p[i]]++;
+                        break;
+                    }
+                }
+                else if(dontCall==false){
+                    if(a[j][i]==op[op.size()-1]){
+                        mp.erase(a[j][i]);
+                        for(k=i;k<m;k++)
+                            a[j][k]=p[i];
+                        mp[p[i]]++;
+                        break;
+                    }
+                }
+            }
+        }
+        for(auto q:mp){
+            if(q.first!=p[i]){
+                mp[q.first]++;
+            }
+        }
+    }
+    int hit=0;
+    vector<int> hitv(m);
 
-// Function to check whether a page exists
-// in a frame or not
-bool search(int key, vector<int>& fr)
-{
-	for (int i = 0; i < fr.size(); i++)
-		if (fr[i] == key)
-			return true;
-	return false;
+    for(i=1;i<m;i++){
+        for(j=0;j<n;j++){
+            if(p[i]==a[j][i-1]){
+                hit++;
+                hitv[i]=1;
+                break;            
+            }
+        }
+    }
+
+    cout<<"Process ";
+    for(i=0;i<m;i++){
+        cout<<p[i]<<" ";
+    }
+    cout<<'\n';
+    for(i=0;i<n;i++){
+        cout<<"Frame "<<i<<" ";
+        for(j=0;j<m;j++){
+            if(a[i][j]==-1)
+                cout<<"E ";
+                else 
+            cout<<a[i][j]<<" ";
+        }
+        cout<<'\n';
+    }
+    cout<<"HIT     ";
+    for(i=0;i<hitv.size();i++){
+        if(hitv[i]==0)
+            cout<<"  ";
+        else
+        cout<<hitv[i]<<" ";
+    }
+    cout<<"\n";
+    cout<<"Hit "<<hit<<'\n'<<"Page Fault "<<m-hit<<'\n';
+    return 0;
 }
-
-// Function to find the frame that will not be used
-// recently in future after given index in pg[0..pn-1]
-int predict(int pg[], vector<int>& fr, int pn, int index)
-{
-	// Store the index of pages which are going
-	// to be used recently in future
-	int res = -1, farthest = index;
-	for (int i = 0; i < fr.size(); i++) {
-		int j;
-		for (j = index; j < pn; j++) {
-			if (fr[i] == pg[j]) {
-				if (j > farthest) {
-					farthest = j;
-					res = i;
-				}
-				break;
-			}
-		}
-
-		// If a page is never referenced in future,
-		// return it.
-		if (j == pn)
-			return i;
-	}
-
-	// If all of the frames were not in future,
-	// return any of them, we return 0. Otherwise
-	// we return res.
-	return (res == -1) ? 0 : res;
-}
-
-void optimalPage(int pg[], int pn, int fn)
-{
-	// Create an array for given number of
-	// frames and initialize it as empty.
-	vector<int> fr;
-
-	// Traverse through page reference array
-	// and check for miss and hit.
-	int hit = 0;
-	for (int i = 0; i < pn; i++) {
-
-		// Page found in a frame : HIT
-		if (search(pg[i], fr)) {
-			hit++;
-			continue;
-		}
-
-		// Page not found in a frame : MISS
-
-		// If there is space available in frames.
-		if (fr.size() < fn)
-			fr.push_back(pg[i]);
-
-		// Find the page to be replaced.
-		else {
-			int j = predict(pg, fr, pn, i + 1);
-			fr[j] = pg[i];
-		}
-	}
-	cout << "No. of hits = " << hit << endl;
-	cout << "No. of misses = " << pn - hit << endl;
-}
-
-// Driver Function
-int main()
-{
-	int pg[] = { 7, 0, 1, 2, 0, 3, 0, 4, 2, 3, 0, 3, 2 };
-	int pn = sizeof(pg) / sizeof(pg[0]);
-	int fn = 4;
-	optimalPage(pg, pn, fn);
-	return 0;
-}
-
-// This code is contributed by Karandeep Singh
-
