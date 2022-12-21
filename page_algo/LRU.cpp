5f53a7a45ceb5fdbf37f1011763f6f5c304e2042
Atharv Vyas <95745167+Mr-okey@users.noreply.github.com>
2022-12-20 21:03:31 -0800

    Update LRU.cpp

--- page_algo/LRU.cpp
+++ page_algo/LRU.cpp
@@ -1,89 +1,86 @@
-//LRU algorithm
-
+//LRU
 
 #include<bits/stdc++.h>
 using namespace std;
-
-// Function to find page faults using indexes
-int pageFaults(int pages[], int n, int capacity)
-{
-	// To represent set of current pages. We use
-	// an unordered_set so that we quickly check
-	// if a page is present in set or not
-	unordered_set<int> s;
-
-	// To store least recently used indexes
-	// of pages.
-	unordered_map<int, int> indexes;
-
-	// Start from initial page
-	int page_faults = 0;
-	for (int i=0; i<n; i++)
-	{
-		// Check if the set can hold more pages
-		if (s.size() < capacity)
-		{
-			// Insert it into set if not present
-			// already which represents page fault
-			if (s.find(pages[i])==s.end())
-			{
-				s.insert(pages[i]);
-
-				// increment page fault
-				page_faults++;
-			}
-
-			// Store the recently used index of
-			// each page
-			indexes[pages[i]] = i;
-		}
-
-		// If the set is full then need to perform lru
-		// i.e. remove the least recently used page
-		// and insert the current page
-		else
-		{
-			// Check if current page is not already
-			// present in the set
-			if (s.find(pages[i]) == s.end())
-			{
-				// Find the least recently used pages
-				// that is present in the set
-				int lru = INT_MAX, val;
-				for (auto it=s.begin(); it!=s.end(); it++)
-				{
-					if (indexes[*it] < lru)
-					{
-						lru = indexes[*it];
-						val = *it;
-					}
-				}
-
-				// Remove the indexes page
-				s.erase(val);
-
-				// insert the current page
-				s.insert(pages[i]);
-
-				// Increment page faults
-				page_faults++;
-			}
-
-			// Update the current page index
-			indexes[pages[i]] = i;
-		}
-	}
-
-	return page_faults;
+int main(){
+    int n,m,i,j,k,hit=0;
+    cout<<"Enter number of frames\n";
+    cin>>n;
+    cout<<"Enter number of processes\n";
+    cin>>m;
+    vector<int> p(m);
+    vector<int> hi(m);
+    cout<<"Enter processes\n";
+    for(i=0;i<m;i++){
+        cin>>p[i];
+    }    
+    vector<vector<int>> a(n);
+    for(i=0;i<n;i++){
+        a[i]=vector<int>(m,-1);
+    }
+    map <int, int> mp;    
+    for(i=0;i<m;i++){
+        vector<pair<int,int>> c;
+        for(auto q: mp){
+            c.push_back({q.second,q.first});
+        }
+        sort(c.begin(),c.end());
+        bool hasrun=false;
+        for(j=0;j<n;j++){
+            if(a[j][i]==p[i]){
+                hit++;
+                hi[i]=1;
+                mp[p[i]]=1;
+                hasrun=true;
+                break;
+            }
+            if(a[j][i]==-1){
+                for(k=i;k<m;k++)
+                    a[j][k]=p[i];
+                mp[p[i]]++;
+                hasrun=true;
+                break;
+            }
+        }
+        if(j==n||hasrun==false){
+            for(j=0;j<n;j++){
+                if(a[j][i]==c[c.size()-1].second){
+                    mp.erase(a[j][i]);
+                    for(k=i;k<m;k++)
+                        a[j][k]=p[i];
+                    mp[p[i]]++;
+                    break;
+                }
+            }
+        }
+        for(auto q:mp){
+            if(q.first!=p[i]){
+                mp[q.first]++;
+            }
+        }
+    }
+    cout<<"Process ";
+    for(i=0;i<m;i++){
+        cout<<p[i]<<" ";
+    }
+    cout<<'\n';
+    for(i=0;i<n;i++){
+        cout<<"Frame "<<i<<" ";
+        for(j=0;j<m;j++){
+            if(a[i][j]==-1)
+                cout<<"E ";
+                else 
+            cout<<a[i][j]<<" ";
+        }
+        cout<<'\n';
+    }
+    for(i=0;i<m;i++){
+        if(hi[i]==0)
+        cout<<"  ";
+        else
+        cout<<hi[i]<<" ";
+    }
+    cout<<"\n";
+    cout<<"Hit "<<hit<<'\n'<<"Page Fault "<<m-hit<<'\n';
+    return 0;
 }
-
-// Driver code
-int main()
-{
-	int pages[] = {7, 0, 1, 2, 0, 3, 0, 4, 2, 3, 0, 3, 2};
-	int n = sizeof(pages)/sizeof(pages[0]);
-	int capacity = 4;
-	cout << pageFaults(pages, n, capacity);
-	return 0;
-}
-
